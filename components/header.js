fb16a8258bfd361f5b7a2adc05457c951a07b1ad
yongsoocho <yongsoocho578@gmail.com>
2023-02-21 23:25:27 +0900

    3-2강 home screen, header 완료 - swiper 시작

--- components/header.js
+++ components/header.js
@@ -1,3 +1,7 @@
+import MovieIcon from "@/mixins/MovieIcon";
+import SearchIcon from "@/mixins/SearchIcon";
+import SeriesIcon from "@/mixins/SeriesIcon";
+import UserIcon from "@/mixins/UserIcon";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -6,11 +10,34 @@ export default function Header() {
 
   if (asPath == "/") {
     return (
-      <div className="flex items-center pl-4 w-screen absolute top-0 z-[999]">
+      <div className="flex justify-between items-center p-4 w-screen absolute top-0 z-[999] bg-black">
         <div>
           <Link href="/">
-            <img src="./netflix_title.png" width="256px" height="auto" />
+            <img src="./netflix_title.png" width="200px" height="auto" />
           </Link>
+        </div>
+
+        <div className="flex">
+          <Link href="/series" className="headerLink">
+            <SeriesIcon />
+            <span>시리즈</span>
+          </Link>
+
+          <Link href="/movies" className="headerLink">
+            <MovieIcon />
+            <span>영화</span>
+          </Link>
+
+          <Link href="/search" className="headerLink">
+            <SearchIcon />
+            <span>검색</span>
+          </Link>
+        </div>
+
+        <div />
+
+        <div className="w-20 h-20 bg-netflix-red p-2 rounded-full hover:cursor-pointer">
+          <UserIcon />
         </div>
       </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Curation from "@/components/curation";
+import PlayIcon from "@/mixins/PlayIcon";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,21 @@ export default function Home() {
     <div>
       <div className="h-[800px] relative overflow-hidden">
         <img src="/main.jpg" className="w-full -translate-y-1/4" />
+
+        <div className="absolute top-2/4 -translate-y-2/4 p-16">
+          <h1 className="text-6xl font-bold mb-8">Movie Title</h1>
+
+          <p className="w-[396px] text-3xl mb-8">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+
+          <div className="flex justify-center items-center bg-white rounded py-2 w-36 cursor-pointer shadow-xl">
+            <PlayIcon />
+
+            <span className="text-xl text-black font-bold">재생</span>
+          </div>
+        </div>
       </div>
 
       <div>
